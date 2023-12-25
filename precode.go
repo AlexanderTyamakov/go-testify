2eceb2410c1cd2c446c79c1cc1062f5606b4f2d5
AlexanderTyamakov <alex.tyamakov@gmail.com>
2023-12-25 18:36:12 +0300

    Added test file

--- precode.go
+++ precode.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
 )
 
 var cafeList = map[string][]string{
@@ -46,41 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-	assert.Equal(t, "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент", responseRecorder.Body.String())
-}
-
-func TestMainHandlerCorrectRequest(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=3&city=moscow", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-}
-
-func TestMainHandlerUnsupportedCity(t *testing.T) {
-	req, err := http.NewRequest("GET", "/cafe?count=3&city=samara", nil)
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Contains(t, responseRecorder.Body.String(), "wrong city value")
 }
